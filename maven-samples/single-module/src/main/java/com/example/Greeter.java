40b8715bb21e4ea9ef99f73db4ef3c91c112e0cb
Guy Doch <guydoch@gmail.com>
2021-09-15 22:37:01 +0300

    YOU build it !!

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,6 +21,6 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Main Method2!!!");
+	  System.out.println("Hello from Main Method3!!!");
   }
 }
